ea4a73d01ee2a072b2143617a0e3166a5e76b27d
Tomek <tomasz.mackiewicz79@gmail.com>
2022-10-26 20:52:11 +0200

    Bootstrap theme

---
+++ drupal/themes/custom/bwcs/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.bwcs = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
